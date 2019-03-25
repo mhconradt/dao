3203af66363d7985971871ca2718eb8929600b18
Max Conradt <max@tilefive.com>
2019-03-24 23:17:14 -0500

    Hopefully this fixes stuff, but I somehow doubt it
    Signed-off-by: Max Conradt <max@tilefive.com>

--- user/userDAO.go
+++ user/userDAO.go
@@ -9,9 +9,9 @@ import (
 
 	//THIRD PARTY
 	"github.com/mhconradt/dao/place"
-	"github.com/mongodb/mongo-go-driver/bson"
-	"github.com/mongodb/mongo-go-driver/mongo"
-	"github.com/mongodb/mongo-go-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
